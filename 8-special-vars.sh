330ab31722aaeeab90a61556c4645991e816dd1f
RaghuVamsiB <raghuvamsibobburu@gmail.com>
2025-09-01 13:47:39 +0530

    started

---
+++ 8-special-vars.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "All variables passed to the script: $@"
+echo "Number of scripts: $0"
+echo "Present working directory: $PWD"
+echo "Home directory: $HOME"
+echo "Number of variables: $#"
