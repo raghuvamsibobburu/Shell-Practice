ed370d6e0cd058cdc15474983cb442397cc1ae52
RaghuVamsiB <raghuvamsibobburu@gmail.com>
2025-09-01 12:41:28 +0530

    started

---
+++ 4-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+PERSON1=$1
+PERSON2=$2
+PERSON3=$3
+
+echo "$PERSON1 : Hi $PERSON2 how are you"
+echo "$PERSON2 : Hello $PERSON1 doing good what about you"
+echo "$PERSON1 : yeah doing good"
+echo "$PERSON2 : what about $PERSON3"
+echo "$PERSON1 : He is also good"
+echo "$PERSON3 : hi $PERSON2 and $PERSON1 how are you"
