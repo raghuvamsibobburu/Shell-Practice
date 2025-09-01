cd1e080d3eefa73e789fbe04e117ceaf8cb87384
RaghuVamsiB <raghuvamsibobburu@gmail.com>
2025-09-01 13:54:49 +0530

    started

--- 8-special-vars.sh
+++ 8-special-vars.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 echo "All variables passed to the script: $@"
-echo "Number of scripts: $0"
+echo "script Name: $0"
 echo "Present working directory: $PWD"
 echo "Home directory: $HOME"
 echo "Number of variables: $#"
+echo "User running the script: $USER"
+echo "PID of the script: $$"
+sleep 10 &
+echo "PID of last command in background: $!"
