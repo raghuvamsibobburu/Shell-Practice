837203632f8ad035027736cf5d04b550299d4685
RaghuVamsiB <raghuvamsibobburu@gmail.com>
2025-09-01 14:24:34 +0530

    started

--- 9-conditions.sh
+++ 9-conditions.sh
@@ -2,11 +2,11 @@
 
 NUMBER=$1
 
-if [ $Number -lt 10 ]
+if [ $NUMBER -lt 10 ]
 then
-  echo "Given $Number is less then 10"
+  echo "Given $NUMBER is less then 10"
 
 else
-  echo "Given $Number is not less then 10"
+  echo "Given $NUMBER is not less then 10"
 fi
 
