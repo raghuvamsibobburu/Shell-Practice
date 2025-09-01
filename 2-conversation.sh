5115e16ca2808a998a7366a8b5d0a01d3514fc23
RaghuVamsiB <raghuvamsibobburu@gmail.com>
2025-09-01 12:21:39 +0530

    started

---
+++ 2-conversation.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+echo "Trump : Hi modi how are you"
+echo "modi : Hello trump doing good what about you"
+echo "trump : yeah doing good"
+echo "modi : what about mask"
+echo "trump : He is also good"
+ech0 "mask : hi modi and trump how are you"
+
