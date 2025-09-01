1800e753791f80baddd1a32df8c341e45381cf97
RaghuVamsiB <raghuvamsibobburu@gmail.com>
2025-09-01 13:34:27 +0530

    started

--- 7-arrays.sh
+++ 7-arrays.sh
@@ -3,3 +3,7 @@
 MOVIES=("court" "pushpa2" "devara" "war2" "coolie")
 
 echo "First movie: ${MOVIES[0]}"
+echo "First movie: ${MOVIES[2]}"
+echo "First movie: ${MOVIES[4]}"
+
+echo "First movie: ${MOVIES[@]}"
